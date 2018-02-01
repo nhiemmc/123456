b4b1670119d159d9219decb5b4701f4a20135d01
sv <you@example.com>
2018-02-01 16:37:25 +0700

    tao chuong trinh.cpp

---
+++ nhiem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int a,b;
+	cout<<"Nhap so thu 1: \n";
+	cin>>a;
+	cout<<"Nhap so thu 2: \n";
+	cin>>b;
+	cout<<"Tong =\n";
+	cin<<a+b;
+	return 0;
+
+}
